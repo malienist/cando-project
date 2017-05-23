df80dd1b6b1a68f619f42893066b3f89060249a3
vithakur <vishal@y7mail.com>
2017-05-23 17:56:27 +1000

    CSV conversion

--- shodan-dl-ioc-interactive.py
+++ shodan-dl-ioc-interactive.py
@@ -8,17 +8,30 @@ import sys
 import time
 import subprocess
 import shlex
-
+#Start the downloading process
 def start_downloader(*varargs):
+    #User input for various options
     dl_proceed_limit=raw_input("Please enter the number of results you want to limit this download to:\n"
-          "(Eg. 10000 results cost one Credit) ")
-
+          "(Eg. 10000 results cost one Credit ) ")
     dl_proceed_category=raw_input("Enter the category:\n(pick one 'malware' OR 'ics'):")
-    dl_proceed_filename=raw_input("Enter the filename for the new report:")
+    dl_proceed_filename=raw_input("Enter the filename for the new report: ")
+    #Used for conversion to CSV later on#Used for conversion to CSV later on
+    suffix=".json.gz"
+    #The actual command used, with all the options
+    subprocess.call(['shodan', 'download', '--limit', dl_proceed_limit, dl_proceed_filename, 'category:', dl_proceed_category])
+    print("Do you want to convert the results into a CSV file?\n (Press y for YES, n for NO: ")
+    #Converting the JSON file into CSV
+    dl_convert=raw_input('')
+    if dl_convert == 'y':
+        subprocess.call(['shodan', 'convert', dl_proceed_filename + suffix , 'csv'])
+    elif dl_convert == 'n':
+        print("Thanks for using this script! End of program.")
 
-    subprocess.call(shlex.split('shodan download --limit '+dl_proceed_limit, + dl_proceed_filename, 'category:'+ dl_proceed_category))
-
-
+    else:
+        print("Input not recognised by the script. Exiting.")
+        exit()
+    return
+#Start of the program - welcome script
 print("Welcome to Shodan IOC Downloader")
 print("#########################################")
 print("You need Shodan CLI Installed in order for this script to run successfully."
